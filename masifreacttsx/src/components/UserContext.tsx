4d0e10e4ef1b7920e5189d27cf189db61330e8f2
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-06-02 18:13:37 +0200

    Update UserContext.tsx

--- masifreacttsx/src/components/UserContext.tsx
+++ masifreacttsx/src/components/UserContext.tsx
@@ -1,1 +1,33 @@
 
+import React, { createContext, useState } from 'react';
+
+interface UserState{
+  loggedinIn: boolean;
+  userid: number;
+  username: string;
+}
+
+type StateContextType = {
+  activeMenu: boolean;
+  setActiveMenu: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const StateContext = createContext<StateContextType>(
+  null as unknown as StateContextType,
+);
+
+type ContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const ContextProvider = ({ children }: ContextProviderProps) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const value = {
+    activeMenu,
+    setActiveMenu,
+  };
+
+  return (
+    <StateContext.Provider value={value}>{children}</StateContext.Provider>
+  );
+};
