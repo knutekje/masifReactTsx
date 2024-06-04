750057b9dbe735d67b7dc57581a337157bcac93e
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-06-04 13:18:26 +0200

    rerolled tabnav

--- masifreacttsx/src/components/TabNav.tsx
+++ masifreacttsx/src/components/TabNav.tsx
@@ -3,21 +3,30 @@ import Report from "./Report";
 import Incident from "./Incident";
 import { FoodItems } from "./FoodItems";
 import { SubmitReport } from "./SubmitReport";
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 
-export const TabNav = ( options: any[] ) =>   {
+export const TabNav =  () =>   {
 return(
-  <Tabs>
-      <TabList>
-        {options.map((item: { id: Key | null | undefined; component: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; })=> (<Tab key={item.id}>{item.component}</Tab>))}
-       
-      </TabList>
-      <TabPanels>
-      {options.map((item: { id: Key | null | undefined; component: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; })=> (<TabPanel key={item.id}>{item.component}</TabPanel>))}
+ 
+<Tabs>
+  <TabList>
+    <Tab>Report</Tab>
+    <Tab>Incident</Tab>
+    <Tab>Fooditems</Tab>
+    <Tab>Submitreport</Tab>
+  </TabList>
 
-        
-      </TabPanels>
-  </Tabs>
+  <TabPanels>
+    
+    <Report/>
+    <Incident/>
+    <FoodItems/>
+    
+    <TabPanel>
+     <SubmitReport/>
+    </TabPanel>
+    
+  </TabPanels>
+</Tabs>
 )
 }
