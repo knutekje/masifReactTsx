64efbf298712350596be24db1b0435eecf4437b6
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-06-30 09:07:58 +0200

    updated

--- masifreacttsx/src/components/Header.tsx
+++ masifreacttsx/src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
 return(
 
 
-<Center><Image marginTop={"5rem"} width={"18rem"} src='logo-no-background.png' marginBottom={"2rem"}/><SelectFood/></Center>
+<Center><Image marginTop={"5rem"} width={"18rem"} src='logo-no-background.png' marginBottom={"2rem"}/></Center>
 
 )}
 
